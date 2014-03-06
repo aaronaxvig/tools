cc9e3aa5be0689ba303b126c262ce01b635a45ca
Aaron Axvig <aaron@dirttech.com>
2014-03-06 15:49:11 -0600

    First problem of the day completed

---
+++ Problem of the Day/Axvigs.Aaron.PotD/X201403/X20140306.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+[assembly: CLSCompliant(true)]
+namespace PotD.X201403
+{
+    public class X20140306
+    {
+        private string key;
+        private static string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public X20140306(string key)
+        {
+            this.key = key;
+        }
+
+        public string Decrypt(string encryptedMessage)
+        {
+            if (encryptedMessage == null)
+            {
+                throw new ArgumentNullException("encryptedMessage");
+            }
+
+            StringBuilder decryptedMessage = new StringBuilder();
+
+            for (int i = 0; i < encryptedMessage.Length; i++)
+            {
+                int messageValue = chars.IndexOf(encryptedMessage[i]);
+                int keyValue = chars.IndexOf(key[i % key.Length]);
+                int index = (messageValue - keyValue) % 26;
+
+                if (index < 0) { index += 26; }
+
+                decryptedMessage.Append(chars[index]);
+            }
+            return decryptedMessage.ToString();
+        }
+
+        public string Encrypt(string messageToEncrypt)
+        {
+            if (messageToEncrypt == null)
+            {
+                throw new ArgumentNullException("messageToEncrypt");
+            }
+
+            StringBuilder encryptedMessage = new StringBuilder();
+
+            for (int i = 0; i < messageToEncrypt.Length; i++)
+            {
+                int messageValue = chars.IndexOf(messageToEncrypt[i]);
+                int keyValue = chars.IndexOf(key[i % key.Length]);
+                int index = (messageValue + keyValue) % 26;
+
+                if (index > 25) { index -= 26; }
+
+                encryptedMessage.Append(chars[index]);
+            }
+
+            return encryptedMessage.ToString();
+        }
+    }
+}
---
+++ Problem of the Day/Axvigs.Aaron.Tests.PotD/X201403/X20140306Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotD.X201403;
+using System;
+
+[assembly: CLSCompliant(true)]
+namespace Axvigs.Aaron.Test.PotD.X201403
+{
+    [TestClass]
+    public class X20140306Tests
+    {
+        [TestMethod]
+        public void DecryptionTest()
+        {
+            X20140306 decrypter = new X20140306("REDDIT");
+            Assert.AreEqual("TODAYISMYBIRTHDAY", decrypter.Decrypt("KSGDGBJQBEQKKLGDG"));
+        }
+
+        [TestMethod]
+        public void EncryptionTest()
+        {
+            X20140306 encrypter = new X20140306("REDDIT");
+            Assert.AreEqual("KSGDGBJQBEQKKLGDG", encrypter.Encrypt("TODAYISMYBIRTHDAY"));
+        }
+    }
+}
