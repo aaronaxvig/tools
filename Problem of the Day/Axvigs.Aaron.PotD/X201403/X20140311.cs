af0f3bfa7aefae6a2d4de2896d3a4120a44c6427
Aaron Axvig <aaron@dirttech.com>
2014-03-11 20:23:35 -0500

    New PotD
    Integer palindrome checker

---
+++ Problem of the Day/Axvigs.Aaron.PotD/X201403/X20140311.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Axvigs.Aaron.PotD.X201403
+{
+    public class X20140311
+    {
+        public static bool IsPalindrome(int number)
+        {
+            List<int> components = new List<int>();
+
+            int remainder = 0;
+            int counter = 0;
+
+            // Put the digits into a list.
+            while (Math.Pow(10, counter) < number)
+            {
+                number -= remainder;
+                remainder = (int)(number % (Math.Pow(10, counter + 1)));
+                components.Add((int)(remainder / Math.Pow(10, counter)));
+                counter++;
+            }
+
+            // Check if the list is a palindrome.
+            for (int i = 0; i < (int) (Math.Floor((double) (components.Count / 2))); i++)
+            {
+                if(components[i] != components[components.Count - 1 - i])
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ Problem of the Day/Axvigs.Aaron.Tests.PotD/X201403/X20140311Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Axvigs.Aaron.PotD.X201403;
+using System;
+
+namespace Axvigs.Aaron.Test.PotD.X201403
+{
+    [TestClass]
+    public class X20140311Tests
+    {
+        [TestMethod]
+        public void PalindromeTest()
+        {
+            Assert.IsTrue(X20140311.IsPalindrome(12344321));
+            Assert.IsTrue(X20140311.IsPalindrome(1234321));
+            Assert.IsTrue(X20140311.IsPalindrome(123040321));
+            Assert.IsTrue(X20140311.IsPalindrome(121));
+            Assert.IsTrue(X20140311.IsPalindrome(1));
+            Assert.IsFalse(X20140311.IsPalindrome(12345321));
+            Assert.IsFalse(X20140311.IsPalindrome(12340321));
+            Assert.IsFalse(X20140311.IsPalindrome(12));
+        }
+    }
+}
