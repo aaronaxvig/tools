53bf5cdf909504112a13f555acc233b98c252b9c
Aaron Axvig <aaron@dirttech.com>
2014-04-18 19:03:37 -0500

    PotD - 2014-03-12

---
+++ Problem of the Day/Axvigs.Aaron.PotD/X201403/X20140312.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Axvigs.Aaron.PotD.X201403
+{
+    public static class X20140312
+    {
+        public static double GetAngle(Time time)
+        {
+            if (time == null)
+            {
+                throw new ArgumentNullException("time");
+            }
+
+            double hourHandPosition = ((double) time.Hour) * (360 / 24);
+            hourHandPosition += (360 / 24) * (((double) time.Minute) / 60);
+
+            double minuteHandPosition = ((double) time.Minute) * (360 / 60);
+
+            double angle = hourHandPosition - minuteHandPosition;
+
+            if (angle < 0)
+            {
+                angle += 360;
+            }
+
+            if (angle > 180)
+            {
+                angle = angle - (2 * (angle - 180));
+            }
+
+            return angle;
+        }
+    }
+
+    public class Time
+    {
+        /// <summary>
+        /// Hours in a range of 0-23
+        /// </summary>
+        public int Hour { get; set; }
+
+        /// <summary>
+        /// Minutes in a range of 0-59
+        /// </summary>
+        public int Minute { get; set; }
+    }
+}
---
+++ Problem of the Day/Axvigs.Aaron.Tests.PotD/X201403/X20140312Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Axvigs.Aaron.PotD.X201403;
+using System;
+
+namespace Axvigs.Aaron.Test.PotD.X201403
+{
+    [TestClass]
+    public class X20140312Tests
+    {
+        [TestMethod]
+        public void ClockHandAngleTest()
+        {
+            Time six = new Time() { Hour = 6, Minute = 0 };
+            Assert.AreEqual(90, X20140312.GetAngle(six));
+
+            Time sixSeventeen = new Time() { Hour = 6, Minute = 17 };
+            Assert.AreEqual(7.75, X20140312.GetAngle(sixSeventeen));
+
+            Time eighteenSeventeen = new Time { Hour = 18, Minute = 17 };
+            Assert.AreEqual(172.25, X20140312.GetAngle(eighteenSeventeen));
+        }
+    }
+}
