2fca9e4f4e62c945c015c9d390e95e3ab27cd8ae
Aaron Axvig <aaron@axvigs.com>
2015-02-18 09:28:31 -0600

    PotD - 2015-02-10

---
+++ Problem of the Day/Axvigs.Aaron.PotD/X201502/X20150210.cs
@@ -0,0 +1,57 @@
+﻿namespace Axvigs.Aaron.PotD.X201502
+{
+    public static class X20150210
+    {
+        public static int GetLuckyNumberCount(int start, int end)
+        {
+            int count = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                string number = i.ToString();
+
+                if (number.Length == 3 || number.Length == 7)
+                {
+                    count++;
+                }
+                else
+                {
+                    int countThrees = 0;
+
+                    foreach (char letter in number)
+                    {
+                        if (letter == '3')
+                        {
+                            countThrees++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    int countSevens = 0;
+
+                    foreach (char letter in number)
+                    {
+                        if (letter == '7')
+                        {
+                            countSevens++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if(countThrees==number.Length || countSevens==number.Length)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Problem of the Day/Axvigs.Aaron.Tests.PotD/X201502/X20150210Tests.cs
@@ -0,0 +1,15 @@
+﻿using Axvigs.Aaron.PotD.X201502;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Axvigs.Aaron.Test.PotD.X201502
+{
+    [TestClass]
+    public class X20150210Tests
+    {
+        [TestMethod]
+        public void LuckyNumbersTest()
+        {
+            Assert.AreEqual(911, X20150210.GetLuckyNumberCount(0, 1000000));
+        }
+    }
+}
